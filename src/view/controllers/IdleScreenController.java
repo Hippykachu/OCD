318f4f76277fea80f0232b2ddcd5931926237e68
t00191729 <hippykachu>
2016-11-24 15:08:38 +0000

    Creation of the repository * Added All the viewing and Controller files

---
+++ src/view/controllers/IdleScreenController.java
@@ -0,0 +1,32 @@
+package view.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IdleScreenController implements Initializable{
+
+    @FXML private TextField createLordLogin;
+    @FXML private TextField createLordPassword;
+    @FXML private TextField createLordPassword2;
+
+    @FXML private TextField connectLordLogin;
+    @FXML private TextField connectLordPassword;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML private void handleCreateLord(ActionEvent actionEvent) {
+
+    }
+
+    @FXML private void handleLoginLord(ActionEvent actionEvent) {
+
+    }
+}
